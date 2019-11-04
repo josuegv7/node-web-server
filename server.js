d543715b5626857a708dde49054a17682711d730
Josue Guzman <josueguzman@Josues-MBP-2.verizon.net>
2019-11-04 17:09:00 -0500

    added heroku script

--- server.js
+++ server.js
@@ -29,17 +29,17 @@ router(app);
 
 //============ MiddleWear =========================
 // Log for request made to the server:
-// app.use((req, res, next)=>{
-//   var now = new Date().toString();
-//   var log = `${now}: ${req.method} ${req.url}`
-//   console.log(`${now}: ${req.method} ${req.url}`);
-//   fs.appendFile("server.log", log + '\n', (err)=>{
-//     if (err){
-//       console.log('Unable to append to Server.log.')
-//     }
-//   })
-//   next();
-// });
+app.use((req, res, next)=>{
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`
+  console.log(`${now}: ${req.method} ${req.url}`);
+  fs.appendFile("server.log", log + '\n', (err)=>{
+    if (err){
+      console.log('Unable to append to Server.log.')
+    }
+  })
+  next();
+});
 
 // PORT for APP:
 app.listen(port, () => {
